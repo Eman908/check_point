1e2b52a7cb6ced41594e2e32643f29047c2c8575
EmanT <tharwateman38@gmail.com>
2026-01-22 22:59:52 +0200

    configure firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:check_point/firebase_options.dart';
 import 'package:check_point/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const CheckPoint());
 }
